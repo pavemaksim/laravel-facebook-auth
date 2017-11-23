5f632b0ca4b4243e298c7995f28ef6483aff3824
Pavemaksim <pavemaksim@gmail.com>
2017-11-24 00:41:18 +0300

    Comments added and code style fixed in SocialFacebookAccount

--- app/SocialFacebookAccount.php
+++ app/SocialFacebookAccount.php
@@ -6,8 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class SocialFacebookAccount extends Model
 {
-    protected $fillable = ['user_id', 'provider_user_id', 'provider', 'token'];
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id', 'provider_user_id', 'provider', 'token'
+    ];
 
+    /**
+     * Get the user associated to the current Social Account
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function user()
     {
         return $this->belongsTo(User::class);
